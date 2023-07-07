4c44550268dd8738548645d45ec5cffcf84ba922
CompuGenius Programs <contact@cgprograms.com>
2023-07-07 14:58:27 -0400

    Added a server_url field

--- client.py
+++ client.py
@@ -24,10 +24,17 @@ class FileEntryWidget(QWidget):
         self.remove_button = QPushButton("Remove")
         self.remove_button.clicked.connect(self.remove_self)
 
-        layout = QHBoxLayout()
-        layout.addWidget(self.file_label)
-        layout.addWidget(self.title_entry)
-        layout.addWidget(self.remove_button)
+        file_layout = QHBoxLayout()
+        file_layout.addWidget(self.file_label)
+        file_layout.addWidget(self.remove_button)
+
+        entry_layout = QHBoxLayout()
+        entry_layout.addWidget(QLabel("Title:"))
+        entry_layout.addWidget(self.title_entry)
+
+        layout = QVBoxLayout()
+        layout.addLayout(file_layout)
+        layout.addLayout(entry_layout)
 
         self.setLayout(layout)
 
@@ -43,13 +50,21 @@ class MainWindow(QMainWindow):
 
         self.file_entries = []
 
-        self.setWindowTitle("File Upload")
+        self.setWindowTitle("Radmash Uploader")
         self.resize(400, 300)
 
         central_widget = QWidget(self)
         self.setCentralWidget(central_widget)
 
         main_layout = QVBoxLayout(central_widget)
+
+        server_url_layout = QHBoxLayout()
+        self.server_url_label = QLabel("Server URL (don't change unless necessary):")
+        self.server_url_entry = QLineEdit()
+        default_server_url = os.getenv('SERVER_URL')
+        self.server_url_entry.setText(default_server_url)
+        server_url_layout.addWidget(self.server_url_label)
+        server_url_layout.addWidget(self.server_url_entry)
 
         self.select_button = QPushButton("Select Files")
         self.select_button.clicked.connect(self.select_files)
@@ -59,6 +74,7 @@ class MainWindow(QMainWindow):
         self.submit_button = QPushButton("Submit")
         self.submit_button.clicked.connect(self.upload_files)
 
+        main_layout.addLayout(server_url_layout)
         main_layout.addWidget(self.select_button)
         main_layout.addLayout(self.files_layout)
         main_layout.addWidget(self.submit_button)
@@ -66,7 +82,7 @@ class MainWindow(QMainWindow):
     def select_files(self):
         file_dialog = QFileDialog()
         file_dialog.setFileMode(QFileDialog.ExistingFiles)
-        file_dialog.setNameFilter("PDF Files (*.pdf)")
+        file_dialog.setNameFilter("Divrei Torah (*.pdf)")
         if file_dialog.exec_():
             file_paths = file_dialog.selectedFiles()
             for file_path in file_paths:
@@ -81,20 +97,35 @@ class MainWindow(QMainWindow):
         self.file_entries.remove(file_entry)
 
     def upload_files(self):
+        server_url = self.server_url_entry.text()  # Get the server URL from the input field
+        if not server_url:
+            QMessageBox.warning(self, "Warning", "Please enter a server URL")
+            return
+
         file_entries_data = []
         files = []
+        empty_fields = False  # Flag to track if any input field is empty
+
         for file_entry in self.file_entries:
             file_path = file_entry.file_path
             file_name = file_path.split("/")[-1]
             title = file_entry.title_entry.text()
+
+            if not title:  # Check if the title field is empty
+                empty_fields = True
+                QMessageBox.warning(self, "Warning", "Please enter a title for all files")
+                break
+
             file_entries_data.append((file_path, file_name, title))
             with open(file_path, 'rb') as file:
                 file_content = file.read()
                 files.append(('file', (file_name, file_content, 'application/pdf')))
 
+        if empty_fields:
+            return
+
         data = {f'title_{i + 1}': title for i, (_, _, title) in enumerate(file_entries_data)}
 
-        server_url = os.getenv('SERVER_URL')
         response = requests.post(server_url + '/upload', files=files, data=data)
 
         if response.status_code == 200:
